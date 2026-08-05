2b1e9394f88e23cd409d9cc276ebef0deea63ff6
kiteto001 <kitetojohn254@gmail.com>
2026-08-05 16:50:18 +0300

    arrange

---
+++ python/kiteto.py
@@ -0,0 +1,17 @@
+import calculator as calc
+
+result = calc.add(5, 3)
+
+print(result)
+
+class car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+Ferarri = car( make="Ferrari", model="F8 Tributo", year=2020)
+
+print(Ferarri.make)
+print(Ferarri.model)
+print(Ferarri.year)
